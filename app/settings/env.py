538e028d415c0498ba80ced418e84e8850e3ea21
bakanovkirill <kirill.bakanov@gmail.com>
2020-03-24 22:11:04 +0200

    Fix settings without .env file

--- app/settings/env.py
+++ app/settings/env.py
@@ -2,8 +2,11 @@ import environ
 
 env = environ.Env(
     # set casting, default value
-    DEBUG=(bool, False),
+    DEBUG=(bool, True),
     CELERY_TASK_ALWAYS_EAGER=(bool, False),
+    BOT_NUMBER_OF_USERS=(int, 5),
+    BOT_MAX_POSTS_PER_USER=(int, 5),
+    BOT_MAX_LIKES_PER_USER=(int, 5),
 )
 base = environ.Path(__file__) - 3
 environ.Env.read_env(env_file=base(".env"))  # reading .env file
